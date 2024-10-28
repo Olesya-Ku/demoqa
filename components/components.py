347015c47a43e1f97b435578fa0bb80f5c7dea92
Olesya <olesia.spb@mail.ru>
2024-10-29 00:05:07 +0300

    Lesson 8. q1

--- components/components.py
+++ components/components.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 from selenium.webdriver.common.by import By
 
 class WebElement:
@@ -10,9 +12,19 @@ class WebElement:
 
     def find_element(self):
         return self.driver.find_element(By.CSS_SELECTOR,self.locator)
+    def find_elements(self):
+        return self.driver.find_elements(By.CSS_SELECTOR,self.locator)
 
     def get_text(self):
         return str(self.find_element().text)
 
     def visible(self):
         return self.find_element().is_displayed
+
+    def check_count_elements(self, count: int) -> bool:
+        if len(self.find_elements()) == count:
+           return True
+        return False
+
+    def send_keys(self, text: str):
+        self.find_element().send_keys(text)
--- conftest.py
+++ conftest.py
@@ -1,11 +1,10 @@
 import pytest
 from selenium import webdriver
 
+
 @pytest.fixture(scope='session')
 def browser():
-    """Создает экземпляр браузера для тестов."""
-    print("Запускаем браузер...")
     driver = webdriver.Chrome()
     yield driver
-    print("Закрываем браузер...")
+    driver.set_window_size(1000, 1000)
     driver.quit()
